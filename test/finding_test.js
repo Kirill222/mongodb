2831b6e11b7331924085a93110124ca72699117f
John Connor <zkmd2012@inbox.ru>
2021-10-23 14:11:27 +0500

    8 MongoDB findOne by ID

--- test/finding_test.js
+++ test/finding_test.js
@@ -2,31 +2,36 @@ const MarioChar = require('../models/mariochar.js')
 
 describe('Finding records', () => {
 
-    //here we again create a document, so that we have smth to find. 
-    //this code will run before the test
+    let char
+    
    beforeEach((done) => {
 
-     let char = new MarioChar({
+     char = new MarioChar({
         name: 'Mario'
     })
     char.save().then(() => {
         assert(char.isNew === false)  
-        done()
     })
 
     done()
 
 
    })
-   //test itself
+   //test: findOne()
     it('Finds one record from the db', (done) => { 
                
         MarioChar.findOne({name: 'Mario'}).then((result) => {
             assert(result.name === 'Mario')  
-            done()
         })
+        done()        
+    })
 
-        done()
-        
+    //test: find by ID
+    it('Finds a record by ID', (done) => { 
+               
+        MarioChar.findOne({_id: char._id}).then((result) => {
+            assert(result._id.toString() === char._id.toString())  //toString - because mongodb's id is an object
+        })
+        done()        
     })
 })
