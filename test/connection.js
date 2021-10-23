fb9e05941e5a2f74ba650f8b9182804ee5613346
John Connor <zkmd2012@inbox.ru>
2021-10-23 13:18:22 +0500

    6 Dropp collection with beforeEach mocha hook before each test

--- test/connection.js
+++ test/connection.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 //connect to the db before tests run
+//before() is a mocha hook
 before((done) => {
     //connect to mongodb
     mongoose.connect('mongodb://localhost/testaroo') //testaroo - the name of the DB. If it does not exist - mongoose creates it
@@ -16,3 +17,11 @@ before((done) => {
     })
 })
 
+//drop the collection before each test. Use beforeEach hook
+//beforeEach runs before every single test
+beforeEach((done) => {
+    //drop the collection
+    mongoose.connection.collections.mariochars.drop().then(() => {
+        done()
+    })
+})
