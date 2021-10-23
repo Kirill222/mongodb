dbf547751496a4ed4eff72f8b7ae9278e30f6c35
John Connor <zkmd2012@inbox.ru>
2021-10-23 12:39:31 +0500

    5 ES6 Promisses library connected to override mongoose's depricated one. Connect to the DB before tests run

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose')
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo') //testaroo - the name of the DB. If it does not exist - mongoose creates it
+//ES6 promises
+mongoose.Promise = global.Promise
 
-mongoose.connection.once('open', () => {
-    console.log('connection has been made...')
-}).on('error', (error) => {
-    console.log('Connection error:', error)
+//connect to the db before tests run
+before((done) => {
+    //connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo') //testaroo - the name of the DB. If it does not exist - mongoose creates it
+
+    mongoose.connection.once('open', () => {
+        console.log('connection has been made...')
+        done()
+    }).on('error', (error) => {
+        console.log('Connection error:', error)
+    })
 })
+
