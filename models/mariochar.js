431daa1507b4b1831d5a3c0519d8953e7d201c81
John Connor <zkmd2012@inbox.ru>
2021-10-23 11:40:06 +0500

    2 Model creating, use of schema

---
+++ models/mariochar.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+//create schema and model
+const MarioCharSchema = new Schema({
+    name: String,
+    weight: Number,
+})
+
+const MarioChar = mongoose.model('mariochar', MarioCharSchema)
